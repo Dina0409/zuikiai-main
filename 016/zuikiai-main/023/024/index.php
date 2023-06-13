6b817fe7167bd3f92d5fc6c52a528991c40b52c8
Inga <ugne04@gmail.com>
2023-06-13 12:51:01 +0300

    024 php

---
+++ 016/zuikiai-main/023/024/index.php
@@ -0,0 +1,37 @@
+<?php
+
+class Kibiras1 {
+    private $akmenuKiekis = 0;
+
+    public function prideti1Akmeni() {
+$this
+    
+}
+public function pridetiDaugAkmenu($kiekis){
+$this->akmenuKiekis+=1;
+}
+public function kiekPririnktiAkmenu() {
+
+}
+}
+$pirmasKibiras = new Kibiras(rand(0; 10));
+$antrasKibiras = new Kibiras(rand(0; 10));
+$treciasKibiras = new Kibiras(rand(0;10));
+echo '<pre>';
+var_dump($pirmasKibiras);
+var_dump($antrasKibiras);
+var_dump($treciasKibiras);
+
+$pirmasKibiras->prideti1Akmeni()
+
+var_dump($pirmasKibiras);
+print_r(get_declared_classes);
+
+class StatinisKibiras {
+public static $kiekis = 10;
+}
+$statinisKibiras = new statinisKibiras();
+
+var_dump($statinisKibiras);
+
+echo statinisKibiras::$kiekis. '<br';
